b99023667aaac407dbba684c6e21a6de2cbe56c0
lyubomirp <lpetkov44@yahoo.com>
2021-04-12 18:41:51 +0300

    Initial commit - single file with 3 hardcoded blocks

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lyubomirp/simple-blockchain
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+type BlockChain struct {
+	Blocks []*Block
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	chain.Blocks = append(chain.Blocks, CreateBlock(data, prevBlock.Hash))
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
+func main() {
+	chain := InitBlockChain()
+
+	chain.AddBlock("First")
+	chain.AddBlock("Second")
+	chain.AddBlock("Third")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Prev Hash: %x\n", block.PrevHash)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
+}
